784705e9f06580bab6c44e912397955fc0eb2a2b
Nikolay Petrov <petrovnn@gmail.com>
2016-04-02 23:01:28 +0300

    вернул бестмапс

--- bg.js
+++ bg.js
@@ -6,9 +6,9 @@
     var host = url_parts[2]; // берем третий элемент этого массива - домен:     http://domain.ru/map
     var host_parts = host.split( '.' ); // разбиваем домен на состовные части точкой
     var host_clean = host_parts[host_parts.length-2]; // берем основной домен (yandex, google, bestmaps)
-    console.log(tab);
+
     if(
-        ['bestmaps', 'openstreetmap', 'here', 'wikimapia'].indexOf(host_clean) ||
+        ['bestmaps', 'openstreetmap', 'here', 'wikimapia'].indexOf(host_clean) != -1 ||
         (host_clean == 'google' && url_parts[3] == 'maps') ||
         (host_clean == 'yandex' && url_parts[3] == 'maps') ||
         (host_clean == 'bing' && url_parts[3] == 'maps')
