b6037dfe153146aba926310294489e599cc28fff
Nikolay Petrov <petrovnn@gmail.com>
2019-03-25 16:08:02 +0300

    Google Analytics

--- popup.js
+++ popup.js
@@ -647,3 +647,23 @@ function onWindowLoad() {
 }
 
 window.onload = onWindowLoad;
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-122809-34']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
+function trackButton(e) {
+    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+  
+var buttons = document.querySelectorAll('button');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', trackButton);
+}
