1c18f16f492144d58f1bbc44d325f2632f5e5fe7
vitalcrazz <zojl@yandex.ru>
2019-03-28 14:16:33 +0300

    err fix

--- bg.js
+++ bg.js
@@ -3,20 +3,22 @@
     chrome.pageAction.setTitle({tabId: tab.id,title:"Map Helper"});
 
     var url = tab.url;
-    var url_parts = url.split( '/' ); // разбиваем весь урл на массив с помощью слеша
-    var host = url_parts[2]; // берем 2 элемент этого массива - домен:     http://domain.ru/map
-    var host_parts = host.split( '.' ); // разбиваем домен на состовные части точкой
-    var host_clean = host_parts[host_parts.length-2]; // берем основной домен (yandex, google, bestmaps)
+    if(url) {
+        var url_parts = url.split('/'); // разбиваем весь урл на массив с помощью слеша
+        var host = url_parts[2]; // берем 2 элемент этого массива - домен:     http://domain.ru/map
+        var host_parts = host.split('.'); // разбиваем домен на состовные части точкой
+        var host_clean = host_parts[host_parts.length - 2]; // берем основной домен (yandex, google, bestmaps)
 
-    if(['bestmaps', 'openstreetmap', 'here', 'wikimapia', 'loadmap', '2gis', 'brouter', 'qwant', 'mapillary', 'waze', 'strava', 'nakarte', 'xn--e1aaps0bc', 'opentopomap', 'navitel'].indexOf(host_clean) != -1
+        if (['bestmaps', 'openstreetmap', 'here', 'wikimapia', 'loadmap', '2gis', 'brouter', 'qwant', 'mapillary', 'waze', 'strava', 'nakarte', 'xn--e1aaps0bc', 'opentopomap', 'navitel'].indexOf(host_clean) != -1
             || /(google.*maps)/.test(url)
             || /(yandex.*maps)/.test(url)
             || /bing.*maps/.test(url)
             || /maps\.sputnik\.ru/.test(url)
             || /mapy\.cz/.test(url)
             || /loc\.alize\.us/.test(url)
-    ) {
-        chrome.pageAction.setIcon({tabId: tab.id, path: "icon16.png"});
+        ) {
+            chrome.pageAction.setIcon({tabId: tab.id, path: "icon16.png"});
+        }
     }
     chrome.pageAction.show(tab.id);
 
